b34b1e5c04d80cb1d3929949efffd6b59a1cb33c
Stoica Valentin <stoicavalentin@Stoicas-MacBook-Pro.local>
2019-01-23 19:56:02 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Stoica Valentin on 23/01/2019.
+//  Copyright © 2019 Stoica Valentin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
